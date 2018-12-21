d8d4ca8bb5c2e2ab3843dd4e73fbb28d7149c360
tgwalker93 <tgwalker93@gmail.com>
2018-12-21 10:46:08 -0800

    updating react router

--- client/src/App.js
+++ client/src/App.js
@@ -10,11 +10,10 @@ const App = () =>
     <Switch>
       <Route exact path="/landing-page" component={landingpage} />
       <Route exact path="/thank-you" component={thankyou} />
-        {/* <Route exact path="/" render={() => (
-          <Redirect to="/landing-page" />
-        )} /> */}
-        <Route component={landingpage} />
-
+      <Route exact path="/" render={() => (
+        <Redirect to="/landing-page" />
+      )} />
+      <Route component={landingpage} />
     </Switch>
   </div>
   </Router>;
